54cb442c94260b8bd7060442c3f52c68ccf90431
julienpoeschl <julienpoeschl@outlook.de>
2025-07-01 10:38:09 +0000

    Implemented Training Plot: - Plots training history (loss, accuracy curves) - Saves into new folder data/plots - Optional save can be turned on/off in config

--- src/music_generation_lstm/config.py
+++ src/music_generation_lstm/config.py
@@ -2,13 +2,9 @@ from typing import Final
 
 # Hyperparameters
 
-SEQUENCE_LENGTH: Final = 100
+SEQUENCE_LENGTH: Final = 16
 
 GENERATION_LENGTH: Final = 50
-
-QUANTIZATION_PRECISION_DELTA_OFFSET: Final = 1/8
-
-QUANTIZATION_PRECISION_DURATION: Final = 1/8
 
 TRAINING_EPOCHS: Final = 1
 
@@ -25,3 +21,8 @@ RESULTS_MIDI_DIR: Final = "data/midi/results"
 MODELS_DIR: Final = "data/models"
 PROCESSED_DIR: Final = "data/processed"
 TOKEN_MAPS_DIR: Final = "data/token_maps"
+PLOT_DIR: Final = "data/plots"
+
+# Debugging or diagnostics
+
+PLOT_TRAINING: Final = True
