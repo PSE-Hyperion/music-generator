a27a3ace56e691edbb0a2cd67b4ce801a9512ed5
Jay3p <jan.porowski@gmx.de>
2025-07-03 19:18:57 +0200

    Small changes in scripts

--- scripts/install_on_fedora.sh
+++ scripts/install_on_fedora.sh
@@ -2,5 +2,3 @@
 sudo dnf upgrade
 sudo dnf install python3.11 python3.11-devel docker
 
-# Configs
-git config core.autocrlf false
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 PROJECTPATH="${HOME}/music-generator"
 
-# Install python system packages
-sudo dnf install python3.11 python3.11-devel
-
 # Install virtual environment
 python3.11 -m venv ${PROJECTPATH}/.venv
 ${PROJECTPATH}/.venv/bin/pip install -U pip
 ${PROJECTPATH}/.venv/bin/pip install -r ../requirements.txt
 
-
+# configs
+git config core.autocrlf false
