1c18c17d3f093e8ff2b60b7064aa9a22d16c8568
Jay3p <jan.porowski@gmx.de>
2025-07-03 15:45:54 +0200

    Fixing script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
+PROJECTPATH="${HOME}/music-generator"
 
 # Install virtual environment
-python -m venv ../.venv
-../.venv/bin/pip install -U pip
-../.venv/bin/pip install -r ../requirements.txt
-PYTHONPATH="${HOME}/music-generator/.venv/bin/python:${PYTHONPATH}"
+python -m venv ${PROJECTPATH}/.venv
+${PROJECTPATH}/.venv/bin/pip install -U pip
+${PROJECTPATH}/.venv/bin/pip install -r ../requirements.txt
 
 # git configuration
 git config core.autocrlf false
