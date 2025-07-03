fb1e6f38743d6d0bc43c5b078da03261a96a7c6d
Jay3p <jan.porowski@gmx.de>
2025-07-03 17:42:25 +0200

    Updating setup for fedora distro

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 PROJECTPATH="${HOME}/music-generator"
 
+# Install python system packages
+sudo dnf install python3.11 python3.11-venv python3.11-pip python3.11-devel
+
 # Install virtual environment
-python -m venv ${PROJECTPATH}/.venv
+python3.11 -m venv ${PROJECTPATH}/.venv
 ${PROJECTPATH}/.venv/bin/pip install -U pip
 ${PROJECTPATH}/.venv/bin/pip install -r ../requirements.txt
 
-# git configuration
-git config core.autocrlf false
 
