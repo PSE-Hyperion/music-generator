fcf6023a3addd7c39c9d9c8ffc4e3c07c1209871
Jay3p <jan.porowski@gmx.de>
2025-07-05 14:25:05 +0200

    Fixing run-compiled script

--- scripts/run-compiled.sh
+++ scripts/run-compiled.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-DOCKER_BUILDKIT = 1
-CONFIG_PATH = "../.docker/docker-compose.yml"
-SERVICE_NAME = "app"
+DOCKER_BUILDKIT=1
+CONFIG_PATH="${HOME}/music-generator/.docker/docker-compose.yml"
+SERVICE_NAME="app"
 
-docker compose -f ${CONFIG_PATH} --rm --build ${SERVICE_NAME}
+docker compose -f ${CONFIG_PATH} run --rm --build ${SERVICE_NAME}
