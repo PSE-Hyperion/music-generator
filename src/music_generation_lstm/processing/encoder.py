a5895792be84141a41986c7f1379432dd83e5905
Jay3p <jan.porowski@gmx.de>
2025-07-11 12:59:00 +0200

    Adding utils for manipulation of dataset

--- src/music_generation_lstm/processing/encoder.py
+++ src/music_generation_lstm/processing/encoder.py
@@ -1,13 +1,17 @@
 from collections.abc import Iterable
 
 import numpy as np
+import tensorflow as tf
 
 from music_generation_lstm.processing.tokenization import tokens
 
 
 def tokens_to_arrays(token_sequences: Iterable[Iterable[tokens.BaseToken]]):
+    for sequence in token_sequences:
+        pass
     array = np.array(next(iter(token_sequences)))
     print(array)
+    tf.data.Dataset.c
 
 def to_tf_dataset(array):
     pass
