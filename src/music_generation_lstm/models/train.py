d5bbab0a4b99187e5ed14a058fe3d8ca3caefd15
Jay3p <jan.porowski@gmx.de>
2025-07-12 16:37:17 +0200

    Adding basic an extensable training method

--- src/music_generation_lstm/models/train.py
+++ src/music_generation_lstm/models/train.py
@@ -1,4 +1,8 @@
+from typing import Generator
+
+from numpy.typing import NDArray
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras.callbacks import History  # type: ignore
 
 from music_generation_lstm.config import TRAINING_BATCH_SIZE, TRAINING_EPOCHS
@@ -88,3 +92,53 @@ def train_model(model: BaseModel, file_paths: list):
 
     if isinstance(history, History):
         plot.plot_training(history, model.model_id)
+
+
+def train_model_sliding(model: BaseModel, *dataset_parts: NDArray):
+    data, starts = dataset_parts[0:2]
+    training_data_gen = sliding_window_generator(data, starts, 10)
+    training_data_signature = {
+        'X': tf.TensorSpec(shape=(10, 6), dtype=tf.uint8), # window size 10, feature dim 6
+        'y': tf.TensorSpec(shape=(1,6), dtype=tf.uint8)
+    }
+
+    dataset = tf.data.Dataset.from_generator(
+        generator = (lambda: training_data_gen),
+        output_signature = training_data_signature
+       )
+    dataset = (
+        dataset
+        .cache()
+        .shuffle(10000)
+        .batch(64)
+        .prefetch(tf.data.AUTOTUNE)
+    )
+    model.model.fit(dataset, epochs=1)
+
+
+def sliding_window_generator(sequences: NDArray, sequence_start_indexes: NDArray[int], window_size: int, target_size = 1, stride = 1):
+    # 'data' is a concatenated array, the indexes store the start and end indexes of the separate sequences
+    # Asserting that sequences are densely packed and the start indexes are matching the sequences
+    assert sequences.shape[0] == sequence_start_indexes[-1] - sequence_start_indexes[0]
+
+    max_sequences = sequence_start_indexes.shape[0] - 1
+    window_with_target_size = window_size + target_size
+
+    sequence_number = 0
+    window_start_idx = sequence_start_indexes[sequence_number]
+    window_with_target_end_idx = window_start_idx + window_with_target_size
+
+    while sequence_number < max_sequences:
+        if window_with_target_end_idx >= sequence_start_indexes[sequence_number + 1]:
+            sequence_number += 1
+            window_start_idx = sequence_start_indexes[sequence_number]
+            window_with_target_end_idx = window_start_idx + window_with_target_size
+            continue
+
+        window_end_idx = window_start_idx + window_size
+        X = sequences[window_start_idx:window_end_idx]
+        y = sequences[window_end_idx:window_with_target_end_idx]
+        yield {'X': X, 'y': y}
+        window_start_idx += stride
+        window_with_target_end_idx += stride
+
