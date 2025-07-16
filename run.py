2586de7be82bc8005c4a27cd5a64c86eec9cf8a9
Julien Pöschl <147189720+julienpoeschl@users.noreply.github.com>
2025-07-17 00:06:06 +0200

    Detokenize Bug Fix (#49)
    * Detokenize Bug Fix
    - Fixed an error in detokenize, where events were grouped into a chord, even though they had different durations
    - Implemented main file in test and added a new configuration in launch, that allows for starting the test main.py with the debugger. Also renamed previous "run our project" to "run music_generation_lstm" to better showcase the difference between the two modes
    - Other small fixes that others have done aswell in their branches
    
    * fromage and ruff in run.py
    
    * Added line that I mistakenly deleted when solving merge conflict
    
    ---------
    
    Co-authored-by: julienpoeschl <julienpoesch@outlook.de>

--- run.py
+++ run.py
@@ -5,4 +5,4 @@ import sys
 CONFIG_PATH = ".docker/docker-compose.yml"
 SERVICE_NAME = "app"
 
-subprocess.check_call(["docker", "compose", "-f", CONFIG_PATH, "run", "--rm", "--build", SERVICE_NAME] + sys.argv[1:])
+subprocess.check_call(["docker", "compose", "-f", CONFIG_PATH, "run", "--rm", "--build", SERVICE_NAME, *sys.argv[1:]])
