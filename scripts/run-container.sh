066eeb23e646a2878422896c88cdfd6b00d5f12f
Jay3p <jan.porowski@gmx.de>
2025-07-17 13:51:39 +0200

    Changing location of env setting

---
+++ scripts/run-container.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+DOCKER_BUILDKIT=1
+PROJECT_PATH="${HOME}/music-generator"
+DOCKER_COMPOSE_PATH="${PROJECT_PATH}/.docker/docker-compose.yml"
+DOCKERFILE_GPU_PATH="${PROJECT_PATH}/.docker/Dockerfile.gpu"
+
+if [[ "$1" != "--gpu" ]]; then
+    echo "Run default docker program"
+    docker compose -f ${DOCKER_COMPOSE_PATH} run --build --rm "app"
+else
+    echo "Run docker program with GPU"
+    shift
+    docker build -f ${DOCKERFILE_GPU_PATH} -t "music-gen:gpu" ${PROJECT_PATH}
+    docker run --gpus all --rm -it music-gen:gpu
+fi
--- src/music_generation_lstm/main.py
+++ src/music_generation_lstm/main.py
@@ -1,5 +1,4 @@
 import logging
-import os
 
 from music_generation_lstm import cli
 from music_generation_lstm.logging_config import setup_logging
@@ -11,18 +10,8 @@ def main():
     """
     setup_logging(level="INFO")
     logging.getLogger("main").info("Starting CLI")
-    init_envs()
 
     cli.start_session()
-
-def init_envs():
-    """ Setting up environment variables for parts of the program """
-
-    # Log level for compiled operations of TensorFlow
-    os.putenv('TF_CPP_MIN_LOG_LEVEL', '2')
-    # TensorFlow options for more performance but less reproducibility
-    os.putenv('TF_ENABLE_ONEDNN_OPTS', '1')
-    os.putenv('TF_USE_CUDNN', '1')
 
 
 # entry point for  script execution
